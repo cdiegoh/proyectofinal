f71f28b54ee269d08c05775c63fc066abff458cd
cdiegoh <cdiegoh44@gmail.com>
2023-08-06 17:56:59 -0300

    agregando proyecto final

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let bthola=document.querySelector("#bthola");
+bthola.addEventListener("click", ()=> bthola.style.backgroundColor= "#af599e")
+let btinic=document.querySelector("#btinic");
+btinic.addEventListener("click", ()=> btinic.style.backgroundColor= "#af599e")
+let btcont=document.querySelector("#btcont");
+btcont.addEventListener("click", ()=> btcont.style.backgroundColor= "#af599e")
+let btcv=document.querySelector("#boton");
+btcv.addEventListener("click", ()=> btcv.style.backgroundColor= "#af59964d", btcv.style.color="#0f0f0f")
+let foto=document.querySelector("#foto");
+foto.addEventListener("click", () => foto.src="/img/fotoradio1.jpg")
